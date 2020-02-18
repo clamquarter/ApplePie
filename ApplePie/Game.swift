7d69fc41ca85149273f18608062747563cc09236
India Doria <indiadoria@gmail.com>
2020-02-18 14:01:02 -0500

    Finished part five.

--- ApplePie/Game.swift
+++ ApplePie/Game.swift
@@ -9,14 +9,26 @@
 import Foundation
 
 struct Game {
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
     var word: String
     var incorrectMovesRemaining: Int
     var guessedLetters: [Character]
-    mutating func playerGuess(letter: Character) {
+    mutating func playerGuessed(letter: Character) {
 guessedLetters.append(letter)
         if !word.contains(letter) {
             incorrectMovesRemaining -= 1
         }
         
 }
+    
 }
--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -35,13 +35,22 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
         scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
         treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    
     }
     @IBAction func buttonTapped(_ sender: UIButton) {
         sender.isEnabled = false
         let letterString = sender.title(for: .normal)!
-        let letter = Character(letterString.lowercased())
+        let letter = Character(letterString.lowercased() )
+        currentGame.playerGuessed(letter: letter)
+        updateUI()
     }
     
 
